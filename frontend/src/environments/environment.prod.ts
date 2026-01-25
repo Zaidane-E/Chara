00469caba9d43602a790c77e42b9b54b69d6fab8
Zaidane-E <31802624+Zaidane-E@users.noreply.github.com>
2026-01-25 15:37:24 -0500

    comments modified

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // Replace with your Azure App Service URL after deployment
   apiUrl: 'https://chara-ahgzd3fbage7aybt.canadacentral-01.azurewebsites.net/api'
 };
